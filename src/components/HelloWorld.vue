ab21d3af8200941c8e40b4b01ee35445b82fdfac
Stefan Peters <Stefan.Peters@gbv.de>
2020-09-02 10:30:22 +0200

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    Color 1: <input v-model="color1" /> Color 2: <input v-model="color2" />
+    <template
+      v-for="i in 10"
+      :key="i">
+      <section
+        :style="`background-color: ${i % 2 == 1 ? color1 : color2};`">
+        {{ i }}
+      </section>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      color1: "#F6F4F4",
+      color2: "#E9E8E1",
+    }
+  },
+  watch: {
+    $route({ query }) {
+      const { color1, color2 } = query
+      if (color1 && color1 != this.color1) {
+        this.color1 = color1
+      }
+      if (color2 && color2 != this.color2) {
+        this.color2 = color2
+      }
+    },
+    color1() {
+      this.$router.push({
+        query: {
+          color1: this.color1,
+        },
+      })
+    },
+    color2() {
+      this.$router.push({
+        query: {
+          color2: this.color2,
+        },
+      })
+    },
+  },
+}
+</script>
+
+<style scoped>
+section {
+  padding: 50px 0;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+const app = createApp(App)
+import './index.css'
+
+// Load and configure vue-router
+import { createRouter, createWebHistory } from "vue-router"
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: import.meta.env.BASE_URL, component: App },
+  ],
+})
+app.use(router)
+
+app.mount('#app')
